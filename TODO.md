c0f9e7efbe45da820e82dcbfff0535842da80556
Xie Yuheng <xyheme@gmail.com>
2021-09-05 18:12:24 +0800

    up

--- TODO.md
+++ TODO.md
@@ -1,2 +1,21 @@
+- Store Blog Posts as HTML Files
+- Route Wildcard Constraints
+- Use Caching for Expensive Operations
+- Use the Filesystem Class to Read a Directory
+- Find a Composer Package for Post Metadata
+- Collection Sorting and Caching Refresher
+
+- Blade: The Absolute Basics
+- Blade Layouts Two Ways
+- A Few Tweaks and
+
+- Environment Files and Database Connections
+- Migrations: The Absolute Basics
+- Eloquent and the Active Record Pattern
+- Make a Post Model and Migration
+- Eloquent Updates and HTML Escaping
+- 3 Ways to Mitigate Mass Assignment Vulnerabilities
+- Route Model Binding
+
 - simple wiki page
 - use gitlab api
---
+++ resources/views/post.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Summer Blog</title>
+  </head>
+  <body>
+
+    <div class="max-w-xl m-auto">
+      <article class="py-4">
+        <h1 class="font-bold py-2 text-lg"><a href="/post">
+          First Post
+        </a></h1>
+
+        <p class="leading-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+      </article>
+
+      <a class="font-bold" href="/">Go Back</a>
+    </div>
+
+
+  </body>
+</html>
