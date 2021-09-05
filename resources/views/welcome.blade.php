edb05d7d7d41dd8c7cbe556da12c0fd0e9630263
Xie Yuheng <xyheme@gmail.com>
2021-09-05 17:41:56 +0800

    up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>summer</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Summer</title>
   </head>
   <body>
-    hi
+    <h1>Hi</h1>
+    hiya!
   </body>
 </html>
