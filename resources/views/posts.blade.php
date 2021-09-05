a2d142c7a34879a5491fb5d8f44cbd774ea9ec0d
Xie Yuheng <xyheme@gmail.com>
2021-09-05 18:02:45 +0800

    up

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,6 +7,7 @@
     <title>Summer Blog</title>
   </head>
   <body>
+
     <article class="py-4 max-w-xl m-auto">
       <h1 class="font-bold py-2 text-lg">First Post</h1>
 
@@ -15,6 +16,12 @@
 
     <article class="py-4 max-w-xl m-auto">
       <h1 class="font-bold py-2 text-lg">Second Post</h1>
+
+      <p class="leading-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </article>
+
+    <article class="py-4 max-w-xl m-auto">
+      <h1 class="font-bold py-2 text-lg">Third Post</h1>
 
       <p class="leading-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </article>
