16aede90336cd7bd3fa251eb7f4cadfb29c59d7b
Xie Yuheng <xyheme@gmail.com>
2021-09-06 23:26:42 +0800

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
     purge: [
         "./resources/**/*.blade.php",
@@ -6,10 +8,27 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                gray: colors.warmGray,
+                orange: colors.orange,
+                amber: colors.amber,
+                lime: colors.lime,
+                emerald: colors.emerald,
+                teal: colors.teal,
+                cyan: colors.cyan,
+                sky: colors.sky,
+                violet: colors.violet,
+                fuchsia: colors.fuchsia,
+                rose: colors.rose,
+            },
+        },
     },
     variants: {
-        extend: {},
+        extend: {
+            opacity: ["disabled"],
+            backgroundColor: ["disabled"],
+        },
     },
     plugins: [],
 }
