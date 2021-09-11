8c33e5fa10d46b7428bac1f0c43c1c7c0d4d820d
Xie Yuheng <xyheme@gmail.com>
2021-09-12 04:08:20 +0800

    up

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use App\Models\Post;
 use App\Models\Category;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/", function () {
-    // DB::listen(function ($query) {
-    //     Log::info($query->sql);
-    // });
-
     return view("posts", [
         "posts" => Post::with("category")->get(),
     ]);
