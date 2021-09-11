270d923b4f553f00cdc8c0932af7991781539fb6
Xie Yuheng <xyheme@gmail.com>
2021-09-12 04:44:55 +0800

    up

--- api/index.php
+++ api/index.php
@@ -1,8 +1,5 @@
 <?php
 
-/**
- * Here is the serverless function entry
- * for deployment with Vercel.
- */
+phpinfo();
 
-require __DIR__ . "/../public/index.php";
+// require __DIR__ . "/../public/index.php";
