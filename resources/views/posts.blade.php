0db6294af2f3a10e6ba3452dfc61b447b3b12f02
Xie Yuheng <xyheme@gmail.com>
2021-09-26 01:39:24 +0800

    up

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,20 +3,7 @@
   @include ("./post-header")
 
   <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    @if ($posts->count())
-      <x-post-featured-card :post="$posts[0]" />
-
-      @if ($posts->count() > 1)
-        <div class="lg:grid lg:grid-cols-6">
-          @foreach ($posts->skip(1) as $post)
-            <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" />
-          @endforeach
-        </div>
-      @endif
-
-    @else
-      <p class="text-center">No post yet.</p>
-    @endif
+    <x-posts-grid :posts="$posts" />
   </main>
 
 </x-layout>
