e3edf546e736c233e8adb84a6f6d864b433587b6
Xie Yuheng <xyheme@gmail.com>
2021-09-26 00:01:56 +0800

    up

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -8,13 +8,11 @@
     <div class="mt-8 flex flex-col justify-between">
       <header>
         <div class="space-x-2">
-          <a href="#"
-             class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-             style="font-size: 10px">Techniques</a>
+          <a href="#" class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
+            style="font-size: 10px">Techniques</a>
 
-          <a href="#"
-             class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-             style="font-size: 10px">Updates</a>
+          <a href="#" class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
+            style="font-size: 10px">Updates</a>
         </div>
 
         <div class="mt-4">
@@ -53,7 +51,7 @@
 
         <div class="min-w-max">
           <a href="#"
-             class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">
+            class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">
             Read More
           </a>
         </div>
