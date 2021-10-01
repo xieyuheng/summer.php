890482500c1dfa5a82d931da29291f100f56d65e
Xie Yuheng <xyheme@gmail.com>
2021-10-01 19:22:02 +0800

    up

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,8 +2,12 @@
 
   @include ("./post-header")
 
-  <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    <x-posts-grid :posts="$posts" />
-  </main>
+  @if ($posts->count())
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+      <x-posts-grid :posts="$posts" />
+    </main>
+  @else
+    <p class="text-center">No posts yet.</p>
+  @endif
 
 </x-layout>
